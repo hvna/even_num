7caa5b89eb783ec6ddc7d6eebbc756a623d3345e
hvna <44810739+hvna@users.noreply.github.com>
2018-11-20 08:23:14 -0700

    Add files via upload

---
+++ even_num.py
@@ -0,0 +1,12 @@
+'''
+using the Keyboard input
+input()
+'''
+
+
+num = int(input("Enter your integer : "))
+
+if num % 2 == 0 :
+    print("Number :", num, " is even")
+else :
+    print("Number :", num, " is odd")
